e5c515e85f6d91994979235fcfb19bc85f27c106
PeasB <adam.uncao@gmail.com>
2015-10-12 14:09:20 -0400

    Conversation System Progress
    Validate Choices.
    Ai Prefabs that Ryan forgot to do....

--- Assets/Scripts/Conversation Scripts/Conversation.cs
+++ Assets/Scripts/Conversation Scripts/Conversation.cs
@@ -11,6 +11,7 @@ public class ConversationManager {
     private Text DialogueUI;
     private int ConvoID;
     private int DialogueLevel;
+    private int ConversationLevel;
     private XmlNode Conversation;
 
 
@@ -53,13 +54,97 @@ public class ConversationManager {
 
 	private void VaildateChoices(int ConversationLevel)
 	{
-		//Vaildate Choices
-		//Remove un-needed choices
-        //If choice removed automaticly choose remaing one.
+
+        //Load Save and Create Variables
+        XmlDocument Save = new XmlDocument();
+        Save.Load("Assets\\Scripts\\SaveGame.xml");
+        bool ChoiceVaild = true;
+        bool ChoiceOneVaild = true;
+        bool ChoiceTwoVaild = false;
+
+      
+        for (int i = 1; i <= 2; i++)
+        {
+            //Condition Is Condition List ex: All Relationship Conditions are under Condition
+            //Example List: For Relationship Condition <Kate>100</Kate> <Matt>50</Matt>
+            foreach (XmlNode Condition in Conversation.SelectSingleNode("Level" + ConversationLevel + "/Choice" + i + "/Conditions").ChildNodes)
+            {
+                //No Conditions
+                if (Condition.Name == "None")
+                    break;
+                #region Condition Checks
+                //Different Condition Checks
+                switch (Condition.Name)
+                {
+                    case "Relationship": //Relationship Requirment
+
+                        string CurrentPlayer = GameObject.FindWithTag("Player").name;
+                       CurrentPlayer = CurrentPlayer.Remove(0, 7); //Removing Player in Game object name. ex: Player Josh -> Josh
+
+                        foreach (XmlNode Person in Condition.ChildNodes)
+                        {
+                            //if Condition does not meet min Relationship
+                            if (!(int.Parse(Save.SelectSingleNode("Relationships/" + CurrentPlayer + "/" + Person.Name).InnerText) >= int.Parse(Person.InnerText)))
+                            {
+                                ChoiceVaild = false;
+                            }
+                        }
+
+                        break;
+
+                    case "InScene": //Character in Scene Requirment
+
+                        foreach (XmlNode Person in Condition.ChildNodes)
+                        {
+                            //If Character does not exist in Scene.
+                            if (!((GameObject.Find("Ai " + Person.Name) != null) == bool.Parse(Person.InnerText)))
+                            {
+                                ChoiceVaild = false;
+                                break;
+                            }
 
 
+                        }
 
-	}
+                        break;
+
+                }
+                #endregion
+
+                if (!ChoiceVaild)
+                {
+                    if (i == 1)
+                        ChoiceOneVaild = false;
+                    else if (i == 2)
+                        ChoiceTwoVaild = false;
+
+                    break;
+                }
+            }
+
+        }
+
+        #region Choice Display Logic
+        if (ChoiceOneVaild && ChoiceTwoVaild)
+        {
+            //Diplay both choices
+        }
+        else if(ChoiceOneVaild && !ChoiceTwoVaild)
+        {
+            //auto choose Choice 1
+        }
+        else if (ChoiceTwoVaild && !ChoiceOneVaild)
+        {
+            //auto choose Choice 2
+        }
+        else
+        {
+            //Move on with out choice
+        }
+
+        #endregion
+
+    }
 
 
     public void ProcessDialogue()
@@ -68,12 +153,16 @@ public class ConversationManager {
         DialogueLevel++;
         
         //Still more Dialogue.
-        if(DialogueLevel <= int.Parse(Conversation.SelectSingleNode("initalDialogue/DialogueCount").InnerText))
+        if(DialogueLevel <= int.Parse(Conversation.SelectSingleNode("initialDialogue/DialogueCount").InnerText))
         {
             //prints Dialogue.
-            DialogueUI.text = Conversation.SelectSingleNode("initalDialogue").ChildNodes[DialogueLevel].InnerText;
+            DialogueUI.text = Conversation.SelectSingleNode("initialDialogue").ChildNodes[DialogueLevel].InnerText;
         }
-    
+        else if (DialogueLevel > int.Parse(Conversation.SelectSingleNode("initialDialogue/DialogueCount").InnerText))
+        {
+            ConversationLevel++;
+            VaildateChoices(ConversationLevel);
+        }
 
 
 
