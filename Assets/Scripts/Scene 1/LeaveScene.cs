aa005a6aaea0e29b7cdf760f0dd21d7c3a6b7c76
PeasB <adam.uncao@gmail.com>
2015-11-08 17:12:42 -0500

    Small Fix For  Ryan

--- Assets/Scripts/Scene 1/LeaveScene.cs
+++ Assets/Scripts/Scene 1/LeaveScene.cs
@@ -6,15 +6,24 @@ public class LeaveScene : MonoBehaviour {
 
     public CameraFollow MainCamera;
     public MoveCar Car;
+    private bool TimerActive = false;
+    public float Timer = 0f;
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
 	
         if(!this.GetComponent<AreaEvent>().ConversationInstance.IsActive && this.GetComponent<AreaEvent>().ConversationInstance.HasRun)
         {
             MainCamera.Smoothing = new Vector2(0, 0);
-            Application.LoadLevel("Scene 2");
+            TimerActive = true;
         }
+
+        if (TimerActive)
+            Timer -= Time.deltaTime;
+
+        if (Timer <= 0)
+            Application.LoadLevel("Scene 2");
+
 
 
 	}
