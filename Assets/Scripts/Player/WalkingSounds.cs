239e18e1126bbe66dfd831579acdea94a46bf238
PeasB <AdamUncao@Adams-MacBook-Pro-5.local>
2015-11-17 19:26:33 -0500

    Walking Sounds Work

---
+++ Assets/Scripts/Player/WalkingSounds.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WalkingSounds : MonoBehaviour {
+
+	string CurrentSurface;
+
+
+	void OnTriggerEnter2D(Collider2D Other)
+	{
+		/*
+		if(CurrentSurface != Other.tag)
+
+		AudioClip Audio;
+
+		switch (Other.tag) {
+		
+		case "Ground Dirt":
+			
+		
+		
+		} */
+
+
+	}
+
+	private bool IsFloor(string Tag)
+	{
+		/*
+		//Ground
+		if (Tag.Substring(0, 6)) 
+		{
+			
+		
+		} */
+
+		return true;
+
+
+	}
+	
+
+
+
+}
