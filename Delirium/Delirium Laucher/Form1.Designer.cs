543e035c23e91d564fcfe4539e111e0bddcd93ec
PeasB <adam.uncao@gmail.com>
2015-11-21 17:45:11 -0500

    Working Game Launcher!
    pulls updates from Server, by comparing if new version exists.

---
+++ Delirium/Delirium Laucher/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Delirium_Laucher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.prgDownloadBar = new System.Windows.Forms.ProgressBar();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.lblActivity = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // prgDownloadBar
+            // 
+            this.prgDownloadBar.Location = new System.Drawing.Point(30, 294);
+            this.prgDownloadBar.Name = "prgDownloadBar";
+            this.prgDownloadBar.Size = new System.Drawing.Size(386, 34);
+            this.prgDownloadBar.TabIndex = 0;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(437, 274);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(161, 70);
+            this.btnPlay.TabIndex = 1;
+            this.btnPlay.Text = "Play!";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // lblActivity
+            // 
+            this.lblActivity.Location = new System.Drawing.Point(30, 331);
+            this.lblActivity.Name = "lblActivity";
+            this.lblActivity.Size = new System.Drawing.Size(386, 13);
+            this.lblActivity.TabIndex = 2;
+            this.lblActivity.Text = "Activity";
+            this.lblActivity.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblActivity.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 356);
+            this.Controls.Add(this.lblActivity);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.prgDownloadBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Delirium Launcher";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar prgDownloadBar;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Label lblActivity;
+    }
+}
+
---
+++ Delirium/Delirium Laucher/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Diagnostics;
+
+namespace Delirium_Laucher
+{
+    public partial class Form1 : Form
+    {
+        //Globals.
+        float ClientVersionNum;
+        string GameRoot = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\Delirium";
+        int ForceUpdateCount;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            ForceUpdateCount = 0; //Allow User to attempt to update normally. (After 3 Forced Updates)
+
+
+            if (File.Exists(GameRoot + "/Version.txt")) //Check if the Version File exists to find out the Clients current version.
+            {
+                StreamReader VersionReader = new StreamReader(GameRoot + "/Version.txt");
+                ClientVersionNum = float.Parse(VersionReader.ReadLine());
+            }
+            else if (!Directory.Exists(GameRoot)) //The Appdata Folder does not exist.
+            {
+                Directory.CreateDirectory(GameRoot);
+                ClientVersionNum = 0;
+            }
+            else
+            {
+                ClientVersionNum = 0;
+            }
+
+            btnPlay.Enabled = false; //Stop user from hitting the button while update is in progress.
+
+            WebClient VersionWC = new WebClient();
+
+            VersionWC.DownloadStringCompleted += VersionWC_DownloadStringCompleted;
+            VersionWC.DownloadStringAsync(new Uri("http://www.peastudios.net/Delirium/CurrentVersion.txt"));
+            lblActivity.Text = "Checking Version";
+            lblActivity.Visible = true;
+
+        }
+
+        private void VersionWC_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            float ServerVersionNum = ClientVersionNum;
+
+            if (e.Error == null)
+             ServerVersionNum = float.Parse(e.Result); //Parsing Server Version.
+                
+
+            if(ServerVersionNum > ClientVersionNum) //Check if the Server Version is newer then the Client Version.
+            {
+                lblActivity.Text = "Downloading Updates";
+                ForceUpdate();
+            }
+            else //No updates attempt to start game.
+            {
+                lblActivity.Text = "No Updates. Starting game.";
+
+                if(File.Exists(GameRoot + "/Delirium.exe"))
+                {
+                    Process.Start(GameRoot + "/Delirium.exe");
+                    Application.Exit();
+                }
+                else if(e.Error != null)
+                {
+                    lblActivity.Text = "No network connection. Please connect to the Internet to download game.";
+                    btnPlay.Enabled = true;
+                }
+                //If Launcher has failed to boot game then force an update.
+                else if (ForceUpdateCount != 3)
+                {
+                    ForceUpdateCount++;
+                    lblActivity.Text = "Could not start game. No game file found. Forcing Update... Attempt " + ForceUpdateCount + " of 3";
+                    ForceUpdate();
+                }
+                else //Could not get a boot even after 3 Forced Updates.
+                {
+                    btnPlay.Enabled = true;
+                    lblActivity.Text = "All 3 Attempts to force Update has failed. Please reinstall the launcher, or Contact the Developer";
+                }
+
+            }
+
+        }
+
+        private void UpdateWC_DownloadFileCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            lblActivity.Text = "Download Complete. Extracting...";
+
+            // Checks if old file or directory exists before deleting.
+            if(File.Exists(GameRoot + "/Delirium.exe"))
+                File.Delete(GameRoot + "/Delirium.exe");
+
+            if(Directory.Exists(GameRoot + "/Delirium_Data"))
+                Directory.Delete(GameRoot + "/Delirium_Data",true);
+
+            if(File.Exists(GameRoot + "/Version.txt"))
+                File.Delete(GameRoot + "/Version.txt");
+
+
+            //Checks if the Zip is folder and extracts game files.
+            if(File.Exists(GameRoot + "Delirium.zip"))
+                ZipFile.ExtractToDirectory(GameRoot + "Delirium.zip", GameRoot);
+
+            lblActivity.Text = "Extraction Complete, Starting game.";
+
+            //Checks if the Game exe exists if so run it.
+            if (File.Exists(GameRoot + "/Delirium.exe"))
+            {
+                Process.Start(GameRoot + "/Delirium.exe");
+                Application.Exit();
+            }
+            else if (e.Error != null) //If errors downloading files.
+            {
+                lblActivity.Text = "No network connection. Please connect to the Internet to download game.";
+                btnPlay.Enabled = true;
+            }
+            //If Launcher has failed to boot game then force an update.
+            else if(ForceUpdateCount != 3)
+            {
+                ForceUpdateCount++; 
+                lblActivity.Text = "Could not start game. No game file found. Forcing Update... Attempt " + ForceUpdateCount + " of 3";
+                ForceUpdate();
+            }
+            else //Could not get a boot even after 3 Forced Updates.
+            {
+                btnPlay.Enabled = true;
+                lblActivity.Text = "All 3 Attempts to force Update has failed. Please reinstall the launcher, or contact the developer.";
+            }
+            
+                
+        }
+
+        private void UpdateWC_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            //Updates the Progress bar with a percetage out of 100.
+            prgDownloadBar.Maximum = 100;
+            prgDownloadBar.Value = e.ProgressPercentage;
+        }
+
+
+        private void ForceUpdate() //Creates a update call.
+        {
+            WebClient UpdateWC = new WebClient();
+
+            UpdateWC.DownloadProgressChanged += UpdateWC_DownloadProgressChanged;
+            UpdateWC.DownloadFileCompleted += UpdateWC_DownloadFileCompleted;
+            UpdateWC.DownloadFileAsync(new Uri("http://www.peastudios.net/Delirium/Delirium.zip"), GameRoot + "Delirium.zip");
+
+        }
+
+    }
+
+
+}
