29f00d7098aca92fe875b85e764eda99678c61b1
PeasB <AdamUncao@Adams-MacBook-Pro-5.local>
2015-12-06 20:00:52 -0500

    Credits work, Scene 1 bug fix, and New Conversation Option

--- Assets/Scripts/Credits/Credits.cs
+++ Assets/Scripts/Credits/Credits.cs
@@ -6,6 +6,7 @@ public class Credits : MonoBehaviour {
 
 	public GUIText[] textElements; // The array of GUIText elements to display and scroll
 	public float scrollSpeed = 0.2f; // The scrolling speed
+	public float Timer = 90.0f;
 
 	void Update () {
 
@@ -13,7 +14,17 @@ public class Credits : MonoBehaviour {
 			{
 				text.transform.Translate(Vector3.up * scrollSpeed); //scroll the text up		 
 			}
+
+		Timer -= Time.deltaTime;
+
+		if (Timer <= 0) 
+		{
+			Application.LoadLevel("Main Menu");
+		}
+
 	}
+
+
 }
 
 
