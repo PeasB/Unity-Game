8c971effdef50c46353947a77e8d0b136b4b1e1f
PeasB <adam.uncao@gmail.com>
2015-12-08 04:00:58 -0500

    Adams Work

---
+++ Assets/KeyFound.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyFound : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(this.GetComponent<StoryPathChecker>().ValueCheck())
+        {
+            GameObject.Find("Area Event find key").GetComponent<AreaEvent>().CanReactivate = false;
+        }
+
+	
+	}
+}
